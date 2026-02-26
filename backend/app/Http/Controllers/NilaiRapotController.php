5b91494b2af19c4b3dd202790e509213ab6708e4
Revan610 <revanakhlf610@gmail.com>
2026-02-26 11:57:37 +0700

    add model, controller and migration

---
+++ backend/app/Http/Controllers/NilaiRapotController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\nilai_rapot;
+use Illuminate\Http\Request;
+
+class NilaiRapotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(nilai_rapot $nilai_rapot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(nilai_rapot $nilai_rapot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, nilai_rapot $nilai_rapot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(nilai_rapot $nilai_rapot)
+    {
+        //
+    }
+}
